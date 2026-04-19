107bbd4c321eeaa5d9f4b2be12b782db8be444c1
Shiyi Liu <128965247+51432@users.noreply.github.com>
2026-04-19 13:07:47 +0800

    Add stage-1 CNVkit WES reference pipeline with Slurm array

---
+++ config/00_config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# CNVkit WES pipeline shared configuration.
+REFERENCE="/data/person/wup/public/liusy_files/reference_genomes/hg38/reference/Homo_sapiens_assembly38.fasta"
+CNVKIT_TARGET_BED="/data/person/wup/public/liusy_files/reference_genomes/hg38/intervals/UCSC_Agilent_exome_regions.hg38.bed"
+CNVKIT_ACCESS_BED="/data/person/wup/public/liusy_files/reference_genomes/hg38/resources/access.hg38.bed"
+
+# Recommended project-level CNVkit pooled-normal reference path.
+CNVKIT_REFERENCE_DEFAULT="/data/person/wup/public/liusy_files/reference_genomes/hg38/resources/cnvkit/wes_reference.cnn"
+
+# Default runtime options.
+CNVKIT_MODE_DEFAULT="wes"
+CNVKIT_STAGE_DEFAULT="build-reference"
+CNVKIT_THREADS_DEFAULT=8
+CNVKIT_MAX_PARALLEL_DEFAULT=8
+
+# Slurm defaults (can be overridden via environment variables before submission).
+SLURM_PARTITION_DEFAULT="compute"
+SLURM_TIME_DEFAULT="24:00:00"
+SLURM_MEM_DEFAULT="16G"

61d7b621acf8566ae8c02518f7aba8fd88d86392
Shiyi Liu <128965247+51432@users.noreply.github.com>
2026-04-19 15:10:31 +0800

    Update CNVkit target BED path to AgilentSureSelectV5 location

--- config/00_config.sh
+++ config/00_config.sh
@@ -2,7 +2,7 @@
 
 # CNVkit WES pipeline shared configuration.
 REFERENCE="/data/person/wup/public/liusy_files/reference_genomes/hg38/reference/Homo_sapiens_assembly38.fasta"
-CNVKIT_TARGET_BED="/data/person/wup/public/liusy_files/reference_genomes/hg38/intervals/UCSC_Agilent_exome_regions.hg38.bed"
+CNVKIT_TARGET_BED="/data/person/wup/public/liusy_files/reference_genomes/hg38/intervals/AgilentSureSelectV5/UCSC_Agilent_exome_regions.hg38.bed"
 CNVKIT_ACCESS_BED="/data/person/wup/public/liusy_files/reference_genomes/hg38/resources/access.hg38.bed"
 
 # Recommended project-level CNVkit pooled-normal reference path.
